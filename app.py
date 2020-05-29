b0fd533625f0a55c10fb74e521f3cbd85f493f75
Venkatesh Prasad R G <venkateshprasad143@gmail.com>
2020-05-29 20:00:30 +0530

    Update app.py to capture request headers for static objs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template, send_file
+from flask import Flask, redirect, url_for, request, render_template, send_file, send_from_directory
 from pymongo import MongoClient
 import datetime
 
@@ -8,6 +8,88 @@ app = Flask(__name__)
 client = MongoClient("mongodb://sampleuser:pass123@ds119020.mlab.com:19020/fladocks")
 # client = MongoClient("mongodb://127.0.0.1:27017/todoapp")
 db = client['fladocks']
+
+@app.route('/sta/<path:path>')
+def getStaticFile(path):
+    fn = str(path).split("/")[-1]
+    file_type = str(fn).split(".")[-1]
+
+    dir = "static"
+    full_path = os.path.join(dir, "logs")
+    if file_type == 'png':
+        req_headers = request.headers
+        filename = full_path + '/request_headers_img.txt'
+
+        if os.path.exists(filename):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        app_logs = open(filename, append_write)
+        app_logs.write("============================" + '\n')
+        app_logs.write(date_time + '\n')
+        app_logs.write("Request Headers: " + '\n' + str(req_headers))
+        app_logs.write("============================" + '\n')
+        app_logs.close()
+
+        # fn = os.path.join(dir, "img") + '/mario-2.png'
+    elif file_type == 'css':
+        req_headers = request.headers
+        filename = full_path + '/request_headers_css.txt'
+
+        if os.path.exists(filename):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        app_logs = open(filename, append_write)
+        app_logs.write("============================" + '\n')
+        app_logs.write(date_time + '\n')
+        app_logs.write("Request Headers: " + '\n' + str(req_headers))
+        app_logs.write("============================" + '\n')
+        app_logs.close()
+
+    elif file_type == 'js':
+        req_headers = request.headers
+        filename = full_path + '/request_headers_js.txt'
+
+        if os.path.exists(filename):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        app_logs = open(filename, append_write)
+        app_logs.write("============================" + '\n')
+        app_logs.write(date_time + '\n')
+        app_logs.write("Request Headers: " + '\n' + str(req_headers))
+        app_logs.write("============================" + '\n')
+        app_logs.close()
+
+    elif file_type == 'html':
+        req_headers = request.headers
+        filename = full_path + '/request_headers_srto.txt'
+
+        if os.path.exists(filename):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        app_logs = open(filename, append_write)
+        app_logs.write("============================" + '\n')
+        app_logs.write(date_time + '\n')
+        app_logs.write("Request Headers: " + '\n' + str(req_headers))
+        app_logs.write("============================" + '\n')
+        app_logs.close()
+
+    return send_from_directory('static', path)
 
 
 @app.route('/')
@@ -35,106 +117,6 @@ def todo():
     items = [item for item in _items]
 
     return render_template('index.html', items=items)
-
-@app.route('/img')
-def img_func():
-    req_headers = request.headers
-    dir = "static"
-    full_path = os.path.join(dir, "logs")
-    filename = full_path +'/request_headers_img.txt'
-
-    if os.path.exists(filename):
-        append_write = 'a'  # append if already exists
-    else:
-        append_write = 'w'  # make a new file if not
-
-    now = datetime.datetime.now()
-    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-    app_logs = open(filename, append_write)
-    app_logs.write("============================" + '\n')
-    app_logs.write(date_time + '\n')
-    app_logs.write("Request Headers: " + '\n' + str(req_headers))
-    app_logs.write("============================"+ '\n')
-    app_logs.close()
-
-    fn = os.path.join(dir, "img") + '/mario-2.png'
-
-    return send_file(fn)
-
-@app.route('/css')
-def css_func():
-    req_headers = request.headers
-    dir = "static"
-    full_path = os.path.join(dir, "logs")
-    filename = full_path +'/request_headers_css.txt'
-
-    if os.path.exists(filename):
-        append_write = 'a'  # append if already exists
-    else:
-        append_write = 'w'  # make a new file if not
-
-    now = datetime.datetime.now()
-    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-    app_logs = open(filename, append_write)
-    app_logs.write("============================" + '\n')
-    app_logs.write(date_time + '\n')
-    app_logs.write("Request Headers: " + '\n' + str(req_headers))
-    app_logs.write("============================"+ '\n')
-    app_logs.close()
-
-    fn = os.path.join(dir, "css") + '/custom.css'
-
-    return send_file(fn)
-
-@app.route('/js')
-def js_func():
-    req_headers = request.headers
-    dir = "static"
-    full_path = os.path.join(dir, "logs")
-    filename = full_path +'/request_headers_js.txt'
-
-    if os.path.exists(filename):
-        append_write = 'a'  # append if already exists
-    else:
-        append_write = 'w'  # make a new file if not
-
-    now = datetime.datetime.now()
-    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-    app_logs = open(filename, append_write)
-    app_logs.write("============================" + '\n')
-    app_logs.write(date_time + '\n')
-    app_logs.write("Request Headers: " + '\n' + str(req_headers))
-    app_logs.write("============================"+ '\n')
-    app_logs.close()
-
-    fn = os.path.join(dir, "js") + '/custom.js'
-
-    return send_file(fn)
-
-@app.route('/srto')
-def srto_func():
-    req_headers = request.headers
-    dir = "static"
-    full_path = os.path.join(dir, "logs")
-    filename = full_path +'/request_headers_srto.txt'
-
-    if os.path.exists(filename):
-        append_write = 'a'  # append if already exists
-    else:
-        append_write = 'w'  # make a new file if not
-
-    now = datetime.datetime.now()
-    date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-    app_logs = open(filename, append_write)
-    app_logs.write("============================" + '\n')
-    app_logs.write(date_time + '\n')
-    app_logs.write("Request Headers: " + '\n' + str(req_headers))
-    app_logs.write("============================"+ '\n')
-    app_logs.close()
-
-    fn = os.path.join(dir, "akamai") + '/sureroute-test-object.html'
-
-    return send_file(fn)
 
 @app.route('/delete-logs/<headers_file>', methods=['GET'])
 def del_logs(headers_file):
